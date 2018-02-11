4b9380292d4420f3ddabfc8b61c3977aa2dfae6d
Greg Coffeng <gcoffeng@gmail.com>
2018-02-10 20:50:44 -0800

    variable name for port simplified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-const gRCalcPort = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-app.listen(gRCalcPort, () => {
-  console.log("GREGS-REACT-CALCULATOR running on port " + gRCalcPort);
+app.listen(port, () => {
+  console.log("GREGS-REACT-CALCULATOR running on port " + port);
 });
